fca04c7c4253395bdc3d7804e75dbd20b7e1c906
Zhjiao <39544608+Zhjiao@users.noreply.github.com>
2019-03-29 18:47:40 +0800

    数值的整数次方
    给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。

---
+++ zhangjiao/Power.java
@@ -0,0 +1,24 @@
+public class Power {
+    public double power(double base,int exponent){
+        if(exponent==0)
+            return 1;
+        if(exponent==1)
+            return base;
+        if(exponent>0) {
+            int key = exponent % 2;
+            if (key == 1)
+                return base * power(base * base, exponent / 2);
+            else
+                return power(base * base, exponent / 2);
+        }
+        else{
+            return 1/power(base,-1*exponent);
+        }
+    }
+        public static void main(String[]args){
+            Power p=new Power();
+            double a=2;int b=-3;
+            System.out.println(p.power(a,b));
+        }
+
+}
